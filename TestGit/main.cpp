1f516210a0ccb9d7ecb3fc4b1807da27ff846570
raspy <raspy@raspy-DREAMPRO>
2015-03-08 19:04:06 +0900

    second commit & push from Ubuntu-PC

---
+++ TestGit/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2015. 3. 8.
+ *      Author: raspy
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "It just for Git Test!" << endl;
+
+	return 0;
+}
